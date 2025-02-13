ded32f40330b57290d707d7212b79a815b1961d7
codechitti216 <codingchitti@gmail.com>
2025-02-13 14:42:35 +0530

    FAN.py updated with new hyperparameters

--- Scripts/Experiments/Architecture_Codes/FAN.py
+++ Scripts/Experiments/Architecture_Codes/FAN.py
@@ -3,47 +3,41 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class FANLayer(nn.Module):
-    def __init__(self, in_features, d_p, d_p_bar, activation=nn.GELU()):
+    def __init__(self, in_features, d_p, d_p_bar, activation):
         super(FANLayer, self).__init__()
         self.Wp = nn.Parameter(torch.randn(in_features, d_p))
         self.Wp_bar = nn.Parameter(torch.randn(in_features, d_p_bar))
         self.Bp_bar = nn.Parameter(torch.zeros(d_p_bar))
-        self.Win = nn.Linear(in_features, in_features, bias=False)  
-        
-        self.projection = nn.Linear(in_features, 2 * d_p + d_p_bar, bias=False)  
-        self.activation = activation  
+        self.Win = nn.Linear(in_features, in_features, bias=False)
+        self.projection = nn.Linear(in_features, 2 * d_p + d_p_bar, bias=False)
+        self.activation = activation
 
     def forward(self, x):
-        if x.dim() == 1:  
-            x = x.unsqueeze(0)  
+        if x.dim() == 1:
+            x = x.unsqueeze(0)
         
-        frequency_scaled_x = self.Win(x)  
+        frequency_scaled_x = self.Win(x)
         cos_term = torch.cos(torch.matmul(frequency_scaled_x, self.Wp))
         sin_term = torch.sin(torch.matmul(frequency_scaled_x, self.Wp))
         non_periodic_term = self.activation(torch.matmul(x, self.Wp_bar) + self.Bp_bar)
-
         output = torch.cat([cos_term, sin_term, non_periodic_term], dim=-1)
-
+        x_proj = self.projection(x)
         
-        x_proj = self.projection(x)  
-        
-        return output + x_proj  
-
+        return output + x_proj
 
 class FAN(nn.Module):
     def __init__(self, number_of_input_neurons=22, number_of_hidden_neurons=60, number_of_output_neurons=3, 
-                 num_layers=3, activation=nn.GELU(), seed_value=16981, learning_rate=1e-3, lambda_reg=0.001):
+                 num_layers=3, activation=nn.GELU(), seed_value=16981, learning_rate=1e-3, lambda_reg=0.001, weight_decay=1e-4):
         super(FAN, self).__init__()
         
         torch.manual_seed(seed_value)
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         
-        self.number_of_input_neurons = number_of_input_neurons
-        self.number_of_hidden_neurons = number_of_hidden_neurons
-        self.number_of_output_neurons = number_of_output_neurons
-        self.num_layers = num_layers
         self.learning_rate = learning_rate
-        self.lambda_reg = lambda_reg  
+        self.lambda_reg = lambda_reg
+        self.num_layers = num_layers
+        self.activation = activation
+        self.weight_decay = weight_decay
         
         self.layers = nn.ModuleList()
         d_p = number_of_hidden_neurons // 4
@@ -51,35 +45,35 @@ class FAN(nn.Module):
         
         for _ in range(num_layers - 1):
             self.layers.append(FANLayer(number_of_input_neurons, d_p, d_p_bar, activation))
-            number_of_input_neurons = 2 * d_p + d_p_bar  
+            number_of_input_neurons = 2 * d_p + d_p_bar
         
         self.WL = nn.Parameter(torch.randn(number_of_input_neurons, number_of_output_neurons))
         self.BL = nn.Parameter(torch.zeros(number_of_output_neurons))
         
         self.prev_output = torch.zeros(number_of_output_neurons, device=self.device)
         
-        self.optimizer = torch.optim.AdamW(self.parameters(), lr=self.learning_rate, weight_decay=1e-4)
+        self.optimizer = torch.optim.AdamW(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
         self.loss_function = nn.MSELoss()
         
         self.to(self.device)
 
     def feedforward(self, input_array):
         x = torch.tensor(input_array, dtype=torch.float32, device=self.device)
-        if x.dim() == 1:  
-            x = x.unsqueeze(0)  
-
+        if x.dim() == 1:
+            x = x.unsqueeze(0)
+        
         for layer in self.layers:
             x = layer(x)
-
-        output = torch.matmul(x, self.WL) + self.BL  
+        
+        output = torch.matmul(x, self.WL) + self.BL
         self.prev_output = output.clone()
-
-        return output.squeeze(0)  
+        
+        return output.squeeze(0)
 
     def compute_loss(self, predictions, targets):
         mse_loss = self.loss_function(predictions, targets)
-        reg_loss = self.lambda_reg * torch.norm(self.WL, p=2)  
-        return mse_loss + reg_loss  
+        reg_loss = self.lambda_reg * torch.norm(self.WL, p=2)
+        return mse_loss + reg_loss
 
     def backpropagate(self, input_array, target_array):
         self.optimizer.zero_grad()
